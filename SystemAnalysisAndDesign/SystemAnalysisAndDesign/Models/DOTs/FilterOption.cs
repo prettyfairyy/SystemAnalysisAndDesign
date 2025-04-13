4caab9b1a3f6cae1f05664ac6451fd84f7e47756
prettyfairy <lyngo704@gmail.com>
2025-04-13 18:07:55 +0700

    Done ViewModel CategoryCarRentViewModel

---
+++ SystemAnalysisAndDesign/SystemAnalysisAndDesign/Models/DOTs/FilterOption.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemAnalysisAndDesign.Models.DTOs
+{
+    public class FilterOption : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
+        }
+
+        public FilterOption(string name, int count, bool isSelected = false)
+        {
+            Name = name;
+            Count = count;
+            IsSelected = isSelected;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
