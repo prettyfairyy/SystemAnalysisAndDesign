d143b57c5c9301840e5632406d664316846fa4ed
prettyfairy <lyngo704@gmail.com>
2025-04-27 16:26:25 +0700

    ViewModel for MainWindow + UI for AdminAddCar

---
+++ SystemAnalysisAndDesign/SystemAnalysisAndDesign/ViewModels/MainWindowViewModel/MainWindowMainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using SystemAnalysisAndDesign.Models;
+using SystemAnalysisAndDesign.Models.Entities;
+using SystemAnalysisAndDesign.ViewModels;
+
+namespace SystemAnalysisAndDesign.ViewModels.MainWindowViewModel
+{
+    public class MainWindowMainViewModel : ViewModelBase
+    {
+        private string _carImage;
+        public string CarImage
+        {
+            get => _carImage;
+            set => SetProperty(ref _carImage, value);
+        }
+
+        public ObservableCollection<Car> CarList { get; set; } // Toàn bộ xe
+        public ObservableCollection<Car> Top3Cars { get; set; } // 4 xe nổi bật
+
+        public MainWindowMainViewModel()
+        {
+            LoadCarsFromDatabase();
+        }
+
+        private void LoadCarsFromDatabase()
+        {
+            using (var context = new RentalDbContext())
+            {
+                var cars = context.Cars.ToList();
+                CarList = new ObservableCollection<Car>(cars);
+                Top3Cars = new ObservableCollection<Car>(cars.Take(3));
+            }
+
+            LoadRandomCarImage();
+        }
+
+        private void LoadRandomCarImage()
+        {
+            if (Top3Cars != null && Top3Cars.Count > 0)
+            {
+                Random random = new Random();
+                int index = random.Next(Top3Cars.Count);
+                CarImage = Top3Cars[index].ImagePath;
+            }
+        }
+    }
+}
